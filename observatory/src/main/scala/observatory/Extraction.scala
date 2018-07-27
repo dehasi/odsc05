4760240ab44be8c9d7af435fdcfdd7174cf22acc
rgaleyev <ravil.galeyev@grandparade.co.uk>
2018-07-27 16:01:31 +0200

    ODSC05-1: Add stations reader

--- observatory/src/main/scala/observatory/Extraction.scala
+++ observatory/src/main/scala/observatory/Extraction.scala
@@ -1,11 +1,42 @@
 package observatory
 
+import java.io.FileReader
+import java.nio.file.Paths
 import java.time.LocalDate
+
+import akka.stream.javadsl.FileIO
+import com.univocity.parsers.csv.{CsvParser, CsvParserSettings}
+
+import scala.io.Source
+//import cats.effect.Sync
+import fs2.{io, text}
 
 /**
   * 1st milestone: data extraction
   */
 object Extraction {
+
+  def fahrenheitToCelsius(f: Double): Double =
+    (f - 32.0) * (5.0 / 9.0)
+
+
+  def toStation(line: String): Station = {
+    def parseLine(line: String): Array[String] = {
+      val array = Array("", "", "", "")
+      var i: Int = 0
+      for (c <- line) {
+        if (c == ',') {
+          i = i + 1
+        } else {
+          array(i) = array(i) + c
+        }
+      }
+      array
+    }
+
+    val parsed = parseLine(line)
+    new Station(parsed(0), parsed(1), Location(if (parsed(2).isEmpty) .0 else parsed(2).toDouble, if (parsed(3).isEmpty) .0 else parsed(3).toDouble))
+  }
 
   /**
     * @param year             Year number
@@ -14,7 +45,9 @@ object Extraction {
     * @return A sequence containing triplets (date, location, temperature)
     */
   def locateTemperatures(year: Year, stationsFile: String, temperaturesFile: String): Iterable[(LocalDate, Location, Temperature)] = {
-    ???
+    val stations = Source.fromFile(stationsFile, "UTF-8").getLines().toStream
+      .map(toStation).toList
+    Seq()
   }
 
   /**
--- observatory/src/test/scala/observatory/ExtractionTest.scala
+++ observatory/src/test/scala/observatory/ExtractionTest.scala
@@ -4,7 +4,10 @@ import org.junit.runner.RunWith
 import org.scalatest.FunSuite
 import org.scalatest.junit.JUnitRunner
 
+@RunWith(classOf[JUnitRunner])
 trait ExtractionTest extends FunSuite {
 
-  
+  test("locateTemperatures reads stations from a file") {
+    val s = Extraction.locateTemperatures(2015, "/Users/rgaleyev/experimental/odsc05/observatory/src/main/resources/stations.csv","2015.csv")
+  }
 }
