bc7ec43faea6d650d0d38cf3bf756330d006d312
rgaleyev <ravil.galeyev@grandparade.co.uk>
2018-07-29 23:54:40 +0200

    ODSC05-1: Implement a basic variant for predictTemperature

--- observatory/src/main/scala/observatory/Visualization.scala
+++ observatory/src/main/scala/observatory/Visualization.scala
@@ -7,18 +7,38 @@ import com.sksamuel.scrimage.{Image, Pixel}
   */
 object Visualization {
 
+  val p: Int = 2
+  val r: Int = 6371
+
+  def distance(from: Location, to: Location): Double = {
+    if (from.lat == to.lat && from.lon == to.lon) 0
+    // todo: check antidotes
+    else r * math.acos(math.sin(from.lat) * math.sin(to.lat) + math.cos(from.lat) * math.cos(to.lat) * math.cos(from.lon - to.lon))
+  }
+
   /**
     * @param temperatures Known temperatures: pairs containing a location and the temperature at this location
-    * @param location Location where to predict the temperature
+    * @param location     Location where to predict the temperature
     * @return The predicted temperature at `location`
     */
   def predictTemperature(temperatures: Iterable[(Location, Temperature)], location: Location): Temperature = {
-    ???
+    val nom = temperatures
+      .map(lt => (distance(location, lt._1), lt._2))
+      .map(dt => (1.0 / math.pow(dt._1, p), dt._2))
+      .map(wt => wt._1 * wt._2)
+      .sum
+
+    val denom = temperatures
+      .map(lt => distance(location, lt._1))
+      .map(dt => 1.0 / math.pow(dt, p))
+      .sum
+
+    nom / denom
   }
 
   /**
     * @param points Pairs containing a value and its associated color
-    * @param value The value to interpolate
+    * @param value  The value to interpolate
     * @return The color that corresponds to `value`, according to the color scale defined by `points`
     */
   def interpolateColor(points: Iterable[(Temperature, Color)], value: Temperature): Color = {
@@ -27,7 +47,7 @@ object Visualization {
 
   /**
     * @param temperatures Known temperatures
-    * @param colors Color scale
+    * @param colors       Color scale
     * @return A 360×180 image where each pixel shows the predicted temperature at its location
     */
   def visualize(temperatures: Iterable[(Location, Temperature)], colors: Iterable[(Temperature, Color)]): Image = {
--- observatory/src/test/scala/observatory/InteractionTest.scala
+++ observatory/src/test/scala/observatory/InteractionTest.scala
@@ -6,5 +6,15 @@ import org.scalatest.prop.Checkers
 import scala.collection.concurrent.TrieMap
 
 trait InteractionTest extends FunSuite with Checkers {
-
+  test("predictTemperature should guess temperature from given dataset") {
+    val dataset: Iterable[(Location, Double)] =
+      Seq(
+        (Location(0.0, 0.0), 0.0),
+        (Location(1.0, 0.0), 0.0),
+        (Location(0.0, 1.0), 10.0),
+        (Location(1.0, 1.0), 10.0)
+      )
+    val result = Visualization.predictTemperature(dataset, Location(0.5, 0.5))
+    assert(result == 5.0)
+  }
 }
